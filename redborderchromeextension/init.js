a653dbbf673d009a9eb9cfd5c9d6a45d055f3885
Andrew Njoo <adnjoo@gmail.com>
2021-06-11 10:14:28 -0700

    fix bug

--- redborderchromeextension/init.js
+++ redborderchromeextension/init.js
@@ -8,19 +8,19 @@ document.body.style.padding = '5px';
 // add event listener to button
 document.addEventListener("DOMContentLoaded", buttonlistener);
 function buttonlistener() {
-    document.getElementById("123").addEventListener('click', handleclick);
+    document.getElementById("button").addEventListener('click', handleclick);
 }
 
 //change border color of all elements to red
 function handleclick() {
     if (!displayBorder) {
         // switch on - sets button border to green - applies red borders
-        document.getElementById("123").innerHTML = "turn off!";
+        document.getElementById("button").innerHTML = "turn off.";
         document.body.style.outline = '5px solid red';
         chrome.tabs.query({"active": true, "currentWindow": true}, handleApplyBorderScript);
         displayBorder = true;
     } else {
-        document.getElementById("123").innerHTML = "turn on!";
+        document.getElementById("button").innerHTML = "turn on.";
         document.body.style.outline = '1px solid grey';
         chrome.tabs.query({"active": true, "currentWindow": true}, handleRemoveBorderScript);
         displayBorder = false;
@@ -29,9 +29,9 @@ function handleclick() {
 
 // runs functions
 function handleApplyBorderScript(tab) {
-    chrome.tabs.executeScript(null, {file: "./apply_border.js"});
+    chrome.tabs.executeScript(null, {file: "./scripts/apply_border.js"});
   }
 
 function handleRemoveBorderScript(tab) {
-    chrome.tabs.executeScript(null, {file: "./remove_border.js"});
+    chrome.tabs.executeScript(null, {file: "./scripts/remove_border.js"});
   }
